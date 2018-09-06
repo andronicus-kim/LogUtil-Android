00b82db62426b3b1a0a7f7daf082615852ca4610
andronicus-kim <androkim.kim7@gmail.com>
2018-09-06 17:02:21 +0300

    add logdebug to log util lib

---
+++ logutil/src/main/java/io/andronicus/logutil/LogDebug.java
@@ -0,0 +1,11 @@
+package io.andronicus.logutil;
+
+import android.util.Log;
+
+public class LogDebug {
+    private static final String TAG = "LogDebug";
+
+    public static void d(String message){
+        Log.d(TAG, message);
+    }
+}
